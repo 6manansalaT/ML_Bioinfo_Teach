66b66add9165a3d7089db68c9c0fdc168fa89a9f
6manansalaT <trinity.joy.manansala@gmail.com>
2026-04-18 11:32:54 -0700

    built on frontend base

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
-print("hi")
+import streamlit as st
+st.title("Bioinfo MLearning")
+
+user_query = st.text_input("What do you want to learn today?", "Enter your question...")
+
+if st.button("Submit"):
+    result = "Success!"
+    st.success(result)
