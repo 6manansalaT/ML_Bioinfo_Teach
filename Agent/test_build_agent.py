8689745b32baaa6531423c9a97821f95dbc03fa7
6manansalaT <trinity.joy.manansala@gmail.com>
2026-04-19 13:20:23 -0700

    connected frontend to backend

--- Agent/test_build_agent.py
+++ Agent/test_build_agent.py
@@ -2,31 +2,33 @@ from langchain_core.prompts import ChatPromptTemplate
 from rich.console import Console
 from rich.markdown import Markdown
 
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are an expert {domain} consultant with 10+ years of experience."),
-    ("human", "Provide a detailed analysis of: {topic} with a focus of educating users on machine learning concepts under bioinformatics tools.")
-])
+def build_agent(user_question):
 
-from model import llm
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "You are an expert {domain} consultant with 10+ years of experience."),
+        ("human", "Provide a detailed analysis of: {topic} with a focus of educating users on machine learning concepts under bioinformatics tools.")
+    ])
 
-# Chain the prompt with the model
-chain = prompt | llm
+    from Agent.model import llm
 
-#user question
-user_question = input("🧬 What ML Bioinformatics topic would you like to learn about today?")
+    # Chain the prompt with the model
+    chain = prompt | llm
 
-# Generate output with specific parameters
-result = chain.invoke({
-    "domain": "bioinformatics, computer science, computational biology, machine learning",
-    "topic": user_question
-})
+    # Generate output with specific parameters
+    result = chain.invoke({
+        "domain": "bioinformatics, computer science, computational biology, machine learning",
+        "topic": user_question
+    })
 
-if isinstance(result.content,list):
-    #join text blocks to avoid text cut off
-    full_text = "".join([block['text'] for block in result.content if block['type'] == 'text'])
-else: 
-    full_text = result.content
+    if isinstance(result.content,list):
+        #join text blocks to avoid text cut off
+        full_text = "".join([block['text'] for block in result.content if block['type'] == 'text'])
+    else: 
+        full_text = result.content
 
-# print with rich
-console = Console()
-console.print(Markdown(full_text))
+    # print with rich
+    console = Console(record=True)
+    console.print(Markdown(full_text))
+    res_string = console.export_text()
+
+    return res_string
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit as st
+import Agent.test_build_agent as test_build_agent
+
 st.title("Bioinfo MLearning")
 
-user_query = st.text_input("What do you want to learn today?", "Enter your question...")
+user_query = st.text_input("🧬 What ML Bioinformatics topic would you like to learn about today?", "Enter text here...")
 
 if st.button("Submit"):
-    result = "Success!"
+    result = test_build_agent.build_agent(user_query)
     st.success(result)
